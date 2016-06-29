b2208846839761e96be8189b57cb7ca52e702661
Manuel Riezebosch <mriezebosch@gmail.com>
2016-06-29 09:20:51 +0200

    Update 3

--- VisualStudio2015Community/Tools/ChocolateyInstall.ps1
+++ VisualStudio2015Community/Tools/ChocolateyInstall.ps1
@@ -1,3 +1,3 @@
 Import-Module (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'VSModules.psm1')
 
-Install-VS 'VisualStudio2015Community' 'http://download.microsoft.com/download/0/B/C/0BC321A4-013F-479C-84E6-4A2F90B11269/vs_community.exe' 'vs_community.exe'
+Install-VS 'VisualStudio2015Community' 'http://download.microsoft.com/download/D/2/3/D23F4D0F-BA2D-4600-8725-6CCECEA05196/vs_community_ENU.exe' 'vs_community.exe'
